0b4e7719445f0fccb2090b2396d9cd4d0325c556
phoenix0010 <apmondal0010@gmail.com>
2020-10-31 11:39:11 +0530

    homepage activity created

---
+++ app/src/main/java/com/example/beliterate/homepage_activity.java
@@ -0,0 +1,14 @@
+package com.example.beliterate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homepage_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage_activity);
+    }
+}
